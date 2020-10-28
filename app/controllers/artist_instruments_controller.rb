610d82736df24b9b896f92b843522af7ea46db4a
Jeff-Adler <Jeffm.adler@gmail.com>
2020-10-28 16:46:51 -0400

    added artist instrument create and index pages

--- app/controllers/artist_instruments_controller.rb
+++ app/controllers/artist_instruments_controller.rb
@@ -1,7 +1,22 @@
 class ArtistInstrumentsController < ApplicationController
+
+    def index 
+        @artist_instruments = ArtistInstrument.all 
+    end
+
     def new
+        @artists = Artist.all
+        @instruments = Instrument.all
     end
 
     def create 
+        ArtistInstrument.create(artist_instrument_params)
+        redirect_to artist_instruments_path
+    end
+
+    private
+
+    def artist_instrument_params
+        params.require(:artist_instrument).permit(:artist_id,:instrument_id)
     end
 end
